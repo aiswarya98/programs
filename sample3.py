a8ea01c553c83043bab2c84be9abd598a0234c5d
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-02 15:46:37 +0530

    Add files via upload

---
+++ sample3.py
@@ -0,0 +1,50 @@
+employee={1:{"id":1000,"details":{"name":"Aiswarya","age":21,"address":"Lingampally"}},
+          2:{"id":1001,"details":{"name":"Sneha","age":21,"address":"Andaman"}},
+          3:{"id":1002,"details":{"name":"Roopa","age":21,"address":"Ongole"}}}
+print(employee)
+
+def accessed_by_executive():
+    x=int(input("Enter employee id:"))
+for i in employee:
+    if employee[id]=x
+    print("ID is present")
+
+def accessed_by_manager():
+    x=int(input("Enter employee id:"))
+for i in employee:
+    if employee[id]=x
+       print(employee[name])
+
+def accessed_by_ceo():
+    x=int(input("Enter employee id:"))
+for i in employee:
+    if employee[id]=x
+      print(employee[details])
+
+def executive():
+    return accessed_by_executive()
+def manager():
+    return accessed_by_manager()
+def ceo():
+    return accessed_by_ceo()
+
+positions= {
+    1:executive,
+    2:manager,
+    3:ceo
+    }
+def switch(position):
+          return positions.get(postion)()
+
+
+
+
+
+    
+
+
+
+
+
+          
+
