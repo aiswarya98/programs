65a12f97be1f456bf48f69d5df897af17db9de92
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-02 14:41:00 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,8 @@
+print("To find whether the given number is +ve,-ve or neutral")
+x=int(input("Enter the number:"))
+if (x>0):
+      print("postive")
+elif (x<0):
+      print("negative")
+else:
+      print("neutral")
