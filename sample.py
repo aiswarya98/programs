4a13fdb6979439be95b8cf2a7da638d5d00d9f56
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-03 09:44:09 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,10 +1,10 @@
 a=float(input())
 b=float(input())
-c=float(input()
+c=float(input())
 if((a>b)and(a>c)):
-    print("a is greater")
-elif((b<a)and(b<c)):
-    print("a is greater")
+    print(a)
+elif((b>a)and(b>c)):
+    print(b)
 else:
-    print("c is greater")
+    print(c)
 
