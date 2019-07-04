8393679b6ccef6a3a440e0815dfd1e817341e480
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-04 11:08:11 +0530

    Update prog16.py

--- prog16.py
+++ prog16.py
@@ -1,8 +1,11 @@
 x,y=map(int,input().split())
+l=[]
 for i in range(x,y):
     if i>1:
         for n in range(2,i):
             if(i%n)==0:
                 break
             else:
-                print(set(i), end=' ')
+                print(i,end=' ')
+                break
+
