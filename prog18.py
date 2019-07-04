b8b3bc80941c3d547ae42f7ffd7e5efd3bcd2f50
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-04 12:06:52 +0530

    Create prog18.py

---
+++ prog18.py
@@ -0,0 +1,10 @@
+x,y=map(int,input().split())
+for i in range(x+1,y):
+    temp=i
+    while i>0:
+        r=temp%10
+        sum=0
+        sum=sum+r**3
+        temp=temp/10
+    if i==sum:
+        print(i,end=' ')
