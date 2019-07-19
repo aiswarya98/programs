4f44d8937a317fc097d1566ed0dc424f8ae9db7a
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-19 14:16:43 +0530

    Create player10.py

---
+++ player10.py
@@ -0,0 +1,9 @@
+x,y=map(int,input().split())
+for num in range(x,y):
+    if(num>1):
+       for i in range(2,num):
+           if(num%i==0):
+               break
+           else:
+              print(num,end=' ')
+              break
