572c33c979e58691891994c65a704eab7cd10ed9
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-19 14:39:00 +0530

    Create player12.py

---
+++ player12.py
@@ -0,0 +1,11 @@
+x=["Monday","Tuesday","Wednesday","Thrusday","Friday"]
+y=["Saturday","Sunday"]
+xy=str(input())
+for i in x:
+    if(xy==i):
+        print("no")
+        break
+    else:
+        print("yes")
+        break
+              
