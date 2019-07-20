c894bb663f42dfd53b06daeb246de2f2c3a7f71e
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-07-20 20:13:53 +0530

    Create player20.py

---
+++ player20.py
@@ -0,0 +1,12 @@
+s=int(input())
+c=0
+t=[]
+n=['a','a','b','i','k','l']
+for j in range (0,s):
+    t.append(input())
+for j in int:
+    f=sorted(j)
+    if(n==f):
+        c=c+1
+print(c)
+        
