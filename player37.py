a05ccd3c3b6fbd6067ba562cd1c5fa1755e63f81
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2019-08-13 10:59:59 +0530

    Update player37.py

--- player37.py
+++ player37.py
@@ -1,5 +1,15 @@
-n=int(input())
-if(n%2) == 0:
-    print("yes")
-else:
-    print("no")
+def isPowerOfTwo(n): 
+    if (n == 0): 
+        return False
+    while (n != 1): 
+            if (n % 2 != 0): 
+                return False
+            n = n // 2
+              
+    return True
+   
+if(isPowerOfTwo(31)): 
+    print('Yes') 
+else: 
+    print('No') 
+    
